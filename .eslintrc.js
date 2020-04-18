c7dbbd5af1a52428cabeea4fc3de9b0b412df063
Pixelastic <tim@pixelastic.com>
2020-04-18 17:38:23 +0200

    fix(lint): Ignore linting errors for missing modules in templates

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,18 +2,11 @@ module.exports = {
   extends: ['./node_modules/aberlaas/lib/configs/eslint.js'],
   overrides: [
     {
+      // requiring sov in templates is expected to fail linting
+      // but should work once copied to the host
       files: ['./templates/**/*.js'],
       rules: {
-        // Allow importing files from sov/ in templates
-        // It is expected to fail in dev, but will correctly resolve once copied
-        // to the host project
-        'import/no-unresolved': [
-          'error',
-          {
-            commonjs: true,
-            ignore: ['^sov/'],
-          },
-        ],
+        'node/no-missing-require': ['off'],
       },
     },
   ],
